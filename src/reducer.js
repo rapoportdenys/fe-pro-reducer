8a75c3b5648fd214436044372990a2d65a523805
Denys <rapoportden@gmail.com>
2021-12-22 13:10:46 +0200

    add reducer function

--- src/reducer.js
+++ src/reducer.js
@@ -1,5 +1,23 @@
-function reducer(input, action) {
-  // Ваше решение тут
-}
+function capitalize(string) {
+  let result = string.split(' ');
+  let newArray = result.map(function (item) {
+      return item.charAt(0).toUpperCase() + item.slice(1);
+  });
 
+  return newArray.join(' ');
+};
+
+function reducer(input, action) {
+  let functionResult;
+  switch (action) {
+      case 'capitalize': functionResult = capitalize(string);
+         
+          break;
+      case 'upperCase': functionResult = string.toUpperCase();
+          break;
+      case 'lowerCase': functionResult = string.toLowerCase();
+          break;
+  }
+  return functionResult;
+}
 module.exports = reducer;
